a816bc66408c1a792d08dfa33117ba858dcaa574
Taybah Tahir <taybah.tahir@madetech.com>
2026-06-02 16:37:37 +0100

    final

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+import os
+import psycopg
+from dotenv import load_dotenv
 from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-import os
-from dotenv import load_dotenv
+
 load_dotenv()
 
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_HOST")
-db_port = os.getenv("DB_PORT")
-db_name = os.getenv("DB_NAME")
-
-connection= f"dbname={db_name} user={db_user} password={db_password} host={db_host} port={db_port}"
+connection = psycopg.connect(
+    host=os.getenv("DB_HOST"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT", "5432")
+)
 
 app = FastAPI()
 templates=Jinja2Templates(directory="templates")
