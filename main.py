cdcc6a17301d5d80a520957c95b98dba7974c3d0
Taybah Tahir <taybah.tahir@madetech.com>
2026-06-02 15:52:48 +0100

    pyschopg

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-connection=None
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+connection= f"dbname={db_name} user={db_user} password={db_password} host={db_host} port={db_port}"
 
 app = FastAPI()
 templates=Jinja2Templates(directory="templates")
@@ -18,4 +27,3 @@ def read_items(request: Request, name: str = "Taybah"):
         name="index.html",
         context={"name": name}
     )
-
