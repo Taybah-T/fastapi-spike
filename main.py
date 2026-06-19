11f5d1fd587b6dd72cff5c415f760bb86d40d5f5
lottie Judge <lottie.judge@madetech.com>
2026-06-19 15:35:13 +0100

    seperated out for clean code, tests passing

--- main.py
+++ main.py
@@ -1,63 +1,99 @@
-from peewee import *
-import os 
-from dotenv import load_dotenv
-from fastapi import FastAPI
-from uuid import uuid4
+from database import db, init_db, Coins, Duties, JoinCoinsAndDuties
+from schemas import CoinCreate
+from typing import List
+from fastapi import FastAPI, HTTPException, status
+from uuid import UUID
 
 app = FastAPI()
-load_dotenv()
 
-db = PostgresqlDatabase(
-    os.getenv("DB_NAME"),
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    port=os.getenv("DB_PORT", "5432")
-)
+@app.on_event("startup")
+def startup():
+    init_db()
 
-class BaseModel(Model):
-    class Meta:
-        database = db
-        schema = "coins"
-
-class Coins(BaseModel):
-    coin_id = UUIDField(primary_key=True, default=uuid4)
-    coin_name = TextField()
-    coin_complete = BooleanField(default=False)
-
-
-class Duties(BaseModel):
-    duty_id = UUIDField(primary_key=True, default=uuid4)
-    duty_name = TextField()
-    duty_description = TextField()
-
-class JoinCoinsAndDuties(BaseModel):
-    coin = ForeignKeyField(Coins)
-    duty = ForeignKeyField(Duties)
-
-db.connect()
-
-# @Dom - best practice is to remove the below, otherwise everytime a dev runs the app it'll remake everything
-# but am leaving in commented to show my working 
-# db.drop_tables([
-#     JoinCoinsAndDuties,
-#     Coins,
-#     Duties
-# ])
-
-db.create_tables([
-    Coins,
-    Duties,
-    JoinCoinsAndDuties
-], safe=True)
-
-# now creating the routes - should the above be seperated out maybe for cc
-# TODO: look into seperation 
 
 # decorator
 @app.get("/coins")
 # method
 def get_coins():
-    coins = Coins.select()
-    return []
+    db.connect(reuse_if_open=True)
 
+    coins_as_list=[]
+    for coin in Coins.select():
+
+        joins = JoinCoinsAndDuties.select().where(JoinCoinsAndDuties.coin == coin)
+        
+        duties_for_coin = []
+        for join in joins:
+            duty_info = {
+                "duty_id": join.duty.duty_id,
+                "duty_name": join.duty.duty_name,
+                "duty_description": join.duty.duty_description
+            }
+            duties_for_coin.append(duty_info)
+
+        coin_info = {
+            "coin_id": coin.coin_id,
+            "coin_name": coin.coin_name,
+            "coin_complete": coin.coin_complete,
+            "duties": duties_for_coin
+        }
+        coins_as_list.append(coin_info)
+    db.close()
+    return coins_as_list
+
+@app.post("/coins", status_code=201)
+def create_coin(payload: CoinCreate):
+    db.connect(reuse_if_open=True)
+
+# validation demonstraton 
+    duplication_check = Coins.select().where(Coins.coin_name == payload.coin_name).exists()
+    if duplication_check == True:
+        db.close()
+        raise HTTPException(status_code=400, detail="Coin name already exists")
+    
+    new_coin = Coins.create(
+        coin_name = payload.coin_name, 
+        coin_complete = payload.coin_complete
+    )
+
+    for duty_id in payload.duty_ids:
+        duty = Duties.get(Duties.duty_id == duty_id)
+
+        JoinCoinsAndDuties.create(coin=new_coin, duty=duty_id)
+
+    db.close()
+    return{"message": "Coin ceated successfully", "coin_id": new_coin.coin_id, "coin_name": new_coin.coin_name}
+
+@app.put("/coins/{coin_id}")
+def update_coin(coin_id: UUID, payload: CoinCreate):
+    db.connect(reuse_if_open=True)
+
+    coin = Coins.get(Coins.coin_id == coin_id)
+
+    coin.coin_name = payload.coin_name
+    coin.coin_complete = payload.coin_complete
+    coin.save()
+
+    remove_duties = JoinCoinsAndDuties.delete().where(JoinCoinsAndDuties.coin == coin)
+    remove_duties.execute()
+
+    for duty_id in payload.duty_ids: 
+        JoinCoinsAndDuties.create(coin = coin, duty =duty_id )
+
+    db.close()
+    return {"message": "coin update successfully"}
+
+# decorator > method
+@app.delete("/coins/{coin_id}")
+def delete_coin(coin_id: UUID):
+    db.connect(reuse_if_open=True)
+
+    coin = Coins.get(Coins.coin_id == coin_id)
+
+    remove_duties = JoinCoinsAndDuties.delete().where(JoinCoinsAndDuties.coin == coin)
+    remove_duties.execute()
+
+    coin.delete_instance()
+
+    db.close()
+    return {"message": "coin deleted successfully"}
--- test_main.py
+++ test_main.py
@@ -1,24 +1,65 @@
 from fastapi.testclient import TestClient
-from main import app, db
+from main import app
+from database import db, Coins, Duties, JoinCoinsAndDuties
 client=TestClient(app)
 
+db.connect(reuse_if_open=True)
+JoinCoinsAndDuties.delete().execute()
+Coins.delete().execute()
+Duties.delete().execute()
+db.close()
 # testing adding a coin
 def test_for_coin():
     response = client.get("/coins")
     assert response.status_code == 200
-
     coins = response.json()
-
     assert isinstance(coins, list)
     
 
 def test_for_adding_coins():
-    response = client.post("/coins", json={"coin_name":"Assemble"})
-    assert response.status_code == 201
+    db.connect(reuse_if_open=True)
+    test_duty = Duties.create(
+        duty_name="Duty 8",
+        duty_description="Evolve and define architecture, utilising the knowledge and experience of the team to design in an optimal user experience, scalability, security, high availability and optimal performance."
+        )
     
-    coins = response.json()
-    print(coins)
+    db.close()
 
-    # assert new coin is in the database 
-    assert [14, "Biscuit"] in coins
+    coin_to_add = {
+        "coin_name": "Assemble",
+        "coin_complete": False,
+        "duty_ids": [str(test_duty.duty_id)]
+    }
+
+    response = client.post("/coins", json=coin_to_add )
+
+    assert response.status_code == 201
+    assert "coin_name" in response.json()
+
+def test_for_no_duplicate_coins():
+    test_coin = {
+          "coin_name": "Assemble",
+        "coin_complete": False,
+        "duty_ids": []
+    }
+
+    response = client.post("/coins", json=test_coin )
+
+    assert response.status_code == 400
+
+
+def test_for_updating_coin():
+    db.connect(reuse_if_open=True)
+    coin = Coins.create(coin_name="Assemble", coin_complete=False)
+    db.close()
+
+    update_coin = {
+        "coin_name": "General Assemble",
+        "coin_complete": True,
+        "duty_ids": []
+    }
+
+    response = client.put(f"/coins/{coin.coin_id}", json=update_coin)
+
+    assert response.status_code == 200
 
